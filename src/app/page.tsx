67f57f91e0d410b35a70f25bdc81edb242ebc6f5
Peter An <75343007+Exotica0122@users.noreply.github.com>
2024-04-16 21:31:50 +1200

    init project with video call example

---
+++ src/app/page.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import {
+  CallControls,
+  CallingState,
+  SpeakerLayout,
+  StreamCall,
+  StreamTheme,
+  StreamVideo,
+  StreamVideoClient,
+  useCallStateHooks,
+  type User,
+} from "@stream-io/video-react-sdk";
+
+import "@stream-io/video-react-sdk/dist/css/styles.css";
+
+const apiKey = "mmhfdzb5evj2"; // the API key can be found in the "Credentials" section
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiS3lsZV9LYXRhcm4iLCJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL0t5bGVfS2F0YXJuIiwiaWF0IjoxNzEzMjMyNDY4LCJleHAiOjE3MTM4MzcyNzN9.vtudLB2P4XTl-NCr-t7MVECY8qQdk5H8zItfbQ92Yeo"; // the token can be found in the "Credentials" section
+const userId = "Kyle_Katarn"; // the user id can be found in the "Credentials" section
+const callId = "7FGfTbTUsbz8"; // the call id can be found in the "Credentials" section
+
+const user: User = {
+  id: userId,
+  name: "Oliver",
+  image: "https://getstream.io/random_svg/?id=oliver&name=Oliver",
+};
+
+const client = new StreamVideoClient({ apiKey, user, token });
+const call = client.call("default", callId);
+call.join({ create: true });
+
+export default function App() {
+  return (
+    <StreamVideo client={client}>
+      <StreamCall call={call}>
+        <MyUILayout />
+      </StreamCall>
+    </StreamVideo>
+  );
+}
+
+export const MyUILayout = () => {
+  const { useCallCallingState } = useCallStateHooks();
+  const callingState = useCallCallingState();
+
+  if (callingState !== CallingState.JOINED) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <StreamTheme>
+      <SpeakerLayout participantsBarPosition="bottom" />
+      <CallControls />
+    </StreamTheme>
+  );
+};
